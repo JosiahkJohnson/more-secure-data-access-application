0b43253bc10d88e17d87630a1c1b3bf20175d625
Josiah <josiahkj123@gmail.com>
2023-05-07 17:17:25 -0500

    pushing up to remote repo

---
+++ Controller.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace data_access_application
+{
+    class Controller
+    {
+        string connection;
+        SqlConnection connect = null;
+
+        public Controller()
+        {
+            connection = "";
+        }
+        public Controller(string connect)
+        {
+            connection = connect;
+        }
+
+        public string getCustomerNumber()
+        {
+            //catching any errors due to authentication so the program keeps running
+            try
+            {
+                int count = 0;
+                connect = new SqlConnection(connection);
+                connect.Open();
+
+                //Old, less secure method
+                //string Query = "SELECT COUNT(*) FROM customers;";
+
+                //new, more secure method
+                string Query = "SELECT [dbo].[GetCustomerCount] ();";
+                SqlCommand command = new SqlCommand(Query, connect);
+                try
+                {
+                    count = Convert.ToInt32(command.ExecuteScalar());
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+
+                connect.Close();
+
+                //if the count was 0, then the user was likely not authorized this information
+                if(count > 0)
+                {
+                    return count.ToString();
+                } else
+                {
+                    //return null so the logic in the rest of the code doesn't throw an extra box at the user
+                    MessageBox.Show("Unable to access this data.");
+                    return null;
+                }
+                
+            } catch
+            {
+                //same as before, return null since there is no data
+                MessageBox.Show("Unable to access this data.");
+                return null;
+            }
+        }
+
+        //get number of employees
+        public string getEmployeeNumber()
+        {
+            int count = 0;
+            connect = new SqlConnection(connection);
+            connect.Open();
+
+            string Query = "SELECT COUNT(*) FROM employees;";
+            SqlCommand command = new SqlCommand(Query, connect);
+
+            
+            try
+            {
+                count = Convert.ToInt32(command.ExecuteScalar());
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            //if the result is 0 the user is likely unauthorized
+            connect.Close();
+            if (count > 0)
+            {
+                return count.ToString();
+            } else
+            {
+                //return null since there is no data to return
+                MessageBox.Show("Unable to access this data.");
+                return null;
+            }
+            
+        }
+
+        //get number of orders
+        public string getOrderNumber()
+        {
+            int count = 0;
+            connect = new SqlConnection(connection);
+            connect.Open();
+
+            string Query = "SELECT COUNT(*) FROM orders;";
+            SqlCommand command = new SqlCommand(Query, connect);
+
+            try
+            {
+                count = Convert.ToInt32(command.ExecuteScalar());
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            //0 means the user likely does not have access to this information
+            connect.Close();
+            if (count > 0)
+            {
+                return count.ToString();
+            } else
+            {
+                //return null since there is no useful data at this point
+                MessageBox.Show("Unable to access this data.");
+                return null;
+            }
+            
+        }
+
+        public string getCustomerNames()
+        {
+            string names = "";
+            SqlDataReader reader;
+            connect = new SqlConnection(connection);
+            connect.Open();
+            //keep the script running, un authorized access will throw an exception
+            try
+            {
+                string Query = "SELECT companyname FROM customers;";
+                SqlCommand command = new SqlCommand(Query, connect);
+                reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    try
+                    {
+                        names = names + reader.GetValue(0) + "\n";
+                        
+                    }
+                    catch (Exception exception)
+                    {
+                        Console.WriteLine(exception.Message);
+                    }
+                }
+                connect.Close();
+                return names.ToString();
+            } catch
+            //exception likely is the user is unauthorzied to this data
+            {
+                MessageBox.Show("Unable to access this data.");
+            }
+
+            //return null is the default assuming the user is unauthorized
+            connect.Close();
+            return null;
+
+        }
+
+        //get the names of all the employees
+        public string getEmployeeNames()
+        {
+            string names = "";
+            SqlDataReader reader;
+            connect = new SqlConnection(connection);
+            connect.Open();
+            //main exception catcher for unauthorized users
+            try
+            {
+                string Query = "SELECT firstname FROM employees;";
+                SqlCommand command = new SqlCommand(Query, connect);
+                reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    try
+                    {
+                        names = names + reader.GetValue(0) + "\n";
+                        
+                    }
+                    catch (Exception exception)
+                    {
+                        Console.WriteLine(exception.Message);
+                    }
+                }
+                connect.Close();
+                return names.ToString();
+            } catch
+            {
+                //default message is likely to happen if user is unauthorzed to this data
+                MessageBox.Show("Unable to access this data.");
+            }
+
+            //this will not execute granted a user is authrized
+            connect.Close();
+            return null;
+        }
+
+        //get all order details
+        public string getOrderDetails()
+        {
+            string names = "";
+            SqlDataReader reader;
+            connect = new SqlConnection(connection);
+            connect.Open();
+
+            //catch unauthorized users
+            try
+            {
+                string Query = "SELECT shipaddress FROM orders;";
+                SqlCommand command = new SqlCommand(Query, connect);
+                reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    try
+                    {
+                        names = names + reader.GetValue(0) + "\n";
+                        
+                    }
+                    catch (Exception exception)
+                    {
+                        Console.WriteLine(exception.Message);
+                    }
+                }
+                connect.Close();
+                return names.ToString();
+            } catch
+            {
+                //default message for unathorized users that break this script
+                MessageBox.Show("Unable to access this data.");
+            }
+            
+            //default return value if authentication fails
+            connect.Close();
+            return null;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace data_access_application
+{
+    public partial class Form1 : Form
+    {
+
+        Controller database;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //connect to the server
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //change to false if authentication fails
+            bool validated = true;
+            
+            //gather information from the user
+            string user = textBox1.Text;
+            string password = textBox2.Text;
+            
+            //must have entered information
+            if(user.Length == 0 || password.Length == 0)
+            {
+                validated = false;
+                MessageBox.Show("Enter Username and Password");
+            } 
+            //cleans the input of user so that only known user values are used
+            else if(!user.Equals("user_ceo", StringComparison.OrdinalIgnoreCase) && !user.Equals("user_sales", StringComparison.OrdinalIgnoreCase) && !user.Equals("user_hr", StringComparison.OrdinalIgnoreCase))
+            {
+                validated = false;
+                MessageBox.Show(user + " not found in the database.");
+            }
+            //detect if the password is not long enough to be considered
+            else if (password.Length < 6)
+            {
+                validated = false;
+                MessageBox.Show("Password entered is invalid.");
+            }
+            else
+            {
+                //final if the password is not all numbers like are required for this program
+                long num1 = 0;
+                if(!long.TryParse(password, out num1))
+                {
+                    validated = false;
+                    MessageBox.Show("Password must be all numbers.");
+                }
+            }
+
+            //finally, if the validation is still valid we can try to connect
+            if(validated)
+            {
+                //insert the information gathered from the user to login
+                MessageBox.Show("Connection info sent");
+                database = new Controller("Server = LAPTOP-1HMCDI96\\SQLEXPRESS;" +
+                                      "Database = Northwind;" +
+                                      "User Id = " + user +
+                                      "; Password = " + password + ";");
+            }
+            
+        }
+
+        //gets the total number of customers
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string count = database.getCustomerNumber();
+            if(count != null)
+            {
+                MessageBox.Show(count + " Customers");
+            }
+
+        }
+
+        //gets all the customer names
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string result = database.getCustomerNames();
+            if(result != null)
+            {
+                MessageBox.Show(result);
+            }
+
+        }
+        //oops
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //gets the total number of employees
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string count = database.getEmployeeNumber();
+            if(count != null)
+            {
+                MessageBox.Show(count + " Employees");
+            }
+  
+        }
+
+        //oops again
+        private void label8_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        //gets the first names of all the employees
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string result = database.getEmployeeNames();
+            if (result != null)
+            {
+                MessageBox.Show(result);
+            }
+  
+        }
+
+        //gets the total number of orders in the system
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string count = database.getOrderNumber();
+            if (count != null)
+            {
+                MessageBox.Show(count + " Orders");
+            }
+  
+        }
+
+        //gets the addresses of the orders in the system
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string result = database.getOrderDetails();
+            if (result != null)
+            {
+                MessageBox.Show("Addresses: \n" + result);
+            }
+   
+        }
+    }
+}
